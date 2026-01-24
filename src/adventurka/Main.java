9766b8939622e33657235e9680972bbe86fbd483
adam <adamdujava@gmail.com>
2026-01-24 16:14:34 +0100

    kod funguje

--- src/adventurka/Main.java
+++ src/adventurka/Main.java
@@ -2,6 +2,7 @@ package adventurka;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        System.setProperty("file.encoding", "UTF-8");
         Engine e = new Engine();
         e.load("src/assets/hra12.json");
         e.play();
